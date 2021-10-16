a845282df2771187fd730e716c43163d0c1dc6d8
User <c.j.dancewilson@outlook.com>
2021-10-16 12:32:41 +1100

    Confirmed list_maker.js accessing correct level of arrays.

--- list_maker.js
+++ list_maker.js
@@ -1,4 +1,6 @@
 d3.json("http://127.0.0.1:5000/featherweight").then((data) => 
     {
-        console.log(data)
-    });
+        data.forEach(element => console.log(element.name))
+    }
+    
+);
